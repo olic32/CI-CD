7d230d4d72ccaff5b04e5b7a594a3bbabfefcd22
olic32 <olicullimore@hotmail.co.uk>
2023-04-05 14:08:51 +0100

    fixed issue

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_home():
-    return "It was the best of times, it was the blurst if times!"
+    return "Hello, world!"
 
 if __name__ == '__main__':
     app.run(
