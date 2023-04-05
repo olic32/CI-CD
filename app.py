833e0558e98294ecb6cd2d52e5c4b0f587010abc
olic32 <olicullimore@hotmail.co.uk>
2023-04-05 14:06:10 +0100

    branch commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_home():
-    return "Hello, world!"
+    return "It was the best of times, it was the blurst if times!"
 
 if __name__ == '__main__':
     app.run(
